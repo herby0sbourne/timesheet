377efa5f576c83cd8922438d708ec1136c48d87b
Herby <herbyosbourne31@gmail.com>
2022-09-29 12:26:36 -0500

    update ignore files and esLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,23 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "prettier",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-  ],
-  overrides: [],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: [
-    "prettier",
-    "react",
-    "@typescript-eslint",
-  ],
-  rules: {
-    "prettier/prettier": ["error"],
-  },
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        "eslint:recommended",
+        "prettier",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+    ],
+    overrides: [],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    plugins: ["prettier", "react", "@typescript-eslint"],
+    rules: {
+        "prettier/prettier": ["warn"],
+        "@typescript-eslint/ban-ts-comment": "off",
+    },
 };
