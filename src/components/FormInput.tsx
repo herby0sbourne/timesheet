d882ab94c031df85d59fc068382f44033bff73ad
Herby <herbyosbourne31@gmail.com>
2022-09-30 10:45:13 -0500

    create FormInput component

---
+++ src/components/FormInput.tsx
@@ -0,0 +1,44 @@
+import React, { ChangeEventHandler } from "react";
+
+type FormInputProps = {
+    label: string;
+    name: string;
+    inputValue: string;
+    displayDate: string[] | string;
+    onChangeHandler: ChangeEventHandler<HTMLInputElement>;
+};
+
+const FromInput = ({
+    label,
+    name,
+    onChangeHandler,
+    inputValue,
+    displayDate,
+}: FormInputProps) => {
+    const textColor = name === "clockIn" ? "text-green-500" : "text-[#e42664]";
+    return (
+        <div className="time-input flex items-center flex-col mb-8">
+            <div className="form-group flex items-center justify-evenly gap-x-7">
+                <label htmlFor="clockIn" className={"inline-block"}>
+                    {label}
+                </label>
+                <input
+                    className={"bg-transparent"}
+                    type="datetime-local"
+                    id={name}
+                    name={name}
+                    onChange={onChangeHandler}
+                    value={inputValue}
+                />
+            </div>
+            <div className="full-date">
+                <span className={"text-gray-400"}>{displayDate[0]}</span>
+                <span>{displayDate[1]}</span>
+                <span className={textColor}>{displayDate[2]}</span>
+                {/*<span className={"text-green-500"}>{displayDate[2]}</span>*/}
+            </div>
+        </div>
+    );
+};
+
+export default FromInput;
