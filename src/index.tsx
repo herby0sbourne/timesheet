7e65fd9d71776480a8ea67dee442c03dc7e09da5
Herby <herbyosbourne31@gmail.com>
2022-10-04 13:35:37 -0500

    code formatting

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import StoreProvider from "./context/Store";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
     <React.StrictMode>
-        <App />
+        <StoreProvider>
+            <App />
+        </StoreProvider>
     </React.StrictMode>
 );
