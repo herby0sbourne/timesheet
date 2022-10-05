483beadec22ea757dc9cfd1b348afd1b819fea24
Herby <herbyosbourne31@gmail.com>
2022-10-05 11:27:44 -0500

    fix font-size to be 18 px

--- src/components/DisplayDate.tsx
+++ src/components/DisplayDate.tsx
@@ -5,7 +5,7 @@ interface DisplayDateProps {
 
 const DisplayDate = ({ displayDate, timeColor }: DisplayDateProps) => {
     return (
-        <div className="full-date text-xl mt-2 mb-16">
+        <div className="full-date text-[18px] mt-2 mb-16">
             <span className={"text-gray-400"}>{displayDate[0]}</span>
             <span>{displayDate[1]}</span>
             <span className={`${timeColor ? "text-green-500" : "text-[#e42664]"}`}>
