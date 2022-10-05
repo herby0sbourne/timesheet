ee8aaf78da0e72865cedf67c753d20e0d6327c30
Herby <herbyosbourne31@gmail.com>
2022-10-05 11:30:57 -0500

    remove CssTransition and unused state

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,36 @@
-import { useRef, useState } from "react";
-import { CSSTransition } from "react-transition-group";
-import Duty from "./components/Duty";
-
+import { useState } from "react";
 import CreateDuty from "./components/CreateDuty";
+import Duty from "./components/Duty";
 
 function App() {
     const [openModal, setOpenModal] = useState(false);
-    const nodeRef = useRef(null);
 
     return (
-        <div className="app">
-            {/*<CreateDuty />*/}
-            <div className="container p-4">
-                <div className="flex justify-between items-center">
-                    <div className="heading">TimeSheet</div>
-                    <div className="hours">$0.00/hour</div>
+        <>
+            <div className="header-info">
+                <div className="container px-4 border-b-2 border-sky-500">
+                    <div className="flex justify-between items-center">
+                        <div className="heading">TimeSheet</div>
+                        <div className="hours">$0.00/hour</div>
+                    </div>
+                    <button
+                        className="create-duty py-2 px-3 text-white text-lg hover:bg-cyan-600 rounded-lg bg-[#101d2c]"
+                        onClick={() => setOpenModal(true)}>
+                        Create Duty
+                    </button>
+                    <div className="total-pay flex justify-between items-center">
+                        <div className="total max-w-[50%] grow"> Total</div>
+                        <div className="hours">24h</div>
+                        <div className="money text-[#5aba8a]">$7,440.00</div>
+                    </div>
                 </div>
-                <button
-                    className="create-duty py-2 px-3 text-white text-lg hover:bg-cyan-600 rounded-lg bg-[#101d2c]"
-                    onClick={() => setOpenModal(true)}>
-                    Create Duty
-                </button>
-                <div className="total-pay flex justify-between items-center border-b-2 border-sky-500">
-                    <div className="total max-w-[50%] grow"> Total</div>
-                    <div className="hours">24h</div>
-                    <div className="money text-[#5aba8a]">$7,440.00</div>
-                </div>
-                <div className="duties-container">
+                <div className="duties-container bg-[#f9f7f6]">
                     <Duty />
                     <Duty />
                 </div>
             </div>
-            <CSSTransition
-                in={openModal}
-                nodeRef={nodeRef}
-                timeout={300}
-                classNames="fade"
-                unmountOnExit
-                onEnter={() => setOpenModal(true)}
-                onExited={() => setOpenModal(false)}>
-                <CreateDuty closeModal={setOpenModal} />
-            </CSSTransition>
-        </div>
+            <CreateDuty closeModal={setOpenModal} openModal={openModal} />
+        </>
     );
 }
 
