d0e9a0424a4ebcfae2ee60bdd462f33a557958ee
Herby <herbyosbourne31@gmail.com>
2022-10-05 11:27:19 -0500

    update css

--- src/components/Duty.tsx
+++ src/components/Duty.tsx
@@ -1,6 +1,6 @@
 const Duty = () => {
     return (
-        <div className="duty bg-[#f9f7f6] mb-2.5">
+        <div className="duty bg-[#f9f7f6] px-4 pt-4 pb-2 border-b-2 border-[#f5deb3]">
             <div className="date flex justify-between items-center">
                 <div className="date-worked">Sun September 25, 2222</div>
                 <div className="cost">$3,744.00</div>
@@ -8,16 +8,14 @@ const Duty = () => {
             <div className="work-hours">
                 <div className="hour flex justify-between items-center">
                     <div className="time-in-out">
-                        <span className="time-in inline-block mr-8 text-[#b28451]">
-                            7:00 AM
-                        </span>
+                        <span className="time-in inline-block mr-8 text-[#b28451]">7:00 AM</span>
                         <span className="time-out inline-block">7:00 PM</span>
                     </div>
                     <div className="total-hours">12h</div>
                 </div>
             </div>
-            <div className="location">
-                <div>Cotton Factory</div>
+            <div className="location font-semibold text-[13px] mt-1 text-gray-400">
+                <div>Location: Cotton Factory</div>
             </div>
         </div>
     );
