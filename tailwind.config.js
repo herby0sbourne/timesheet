6f7f1439af13cfec6ce71801bdc87104ec8c0c7a
Herby <herbyosbourne31@gmail.com>
2022-10-05 13:19:36 -0500

    add custom config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [ './src/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {
+            backgroundImage: {
+                "gradient-radial":
+                    "radial-gradient(ellipse farthest-side at bottom center, #ffe977 0%, #da991c 245%)"
+            }
+        }
+    },
+    plugins: []
+};
