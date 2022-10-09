34f4f8d0c1d14356ae8f02082b81369c55c1875a
Herby <herbyosbourne31@gmail.com>
2022-10-08 20:42:24 -0500

    use custom gradient

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ function App() {
 
     return (
         <>
-            <div className="header-info relative">
-                <div className="container px-4 bg-gradient-radial h-[16vh]">
+            <div className="header-info relative bg-gradient-radial">
+                <div className="container px-4 m-auto h-[16vh]">
                     <div className="flex justify-between items-center">
                         <div className="heading">TimeSheet</div>
                         <div className="hours">$0.00/hour</div>
@@ -18,7 +18,7 @@ function App() {
                         onClick={() => setOpenModal(true)}>
                         Create Duty
                     </button>
-                    <div className="total-pay bg-white shadow-[0px_7px_13px_-6px_rgba(0,0,0,0.28);] translate-y-1/2 flex justify-between items-center absolute bottom-0 right-0 left-0 p-4 z-[2] m-auto w-[calc(100%-1rem)] rounded-[8px]">
+                    <div className="total-pay bg-white shadow-[0px_7px_13px_-6px_rgba(0,0,0,0.28);] translate-y-1/2 flex justify-between items-center absolute bottom-0 right-0 left-0 px-4 py-6 z-[2] m-auto w-[calc(100%-1rem)] rounded-[8px] font-medium tracking-wider">
                         <div className="total max-w-[50%] grow"> Total</div>
                         <div className="hours">24h</div>
                         <div className="money text-[#5aba8a]">$7,440.00</div>
