4349bdc6f73841baa870741bfead10c662d43103
Herby <herbyosbourne31@gmail.com>
2022-10-10 14:07:15 -0500

    create Home Component to show list of jobs

---
+++ src/components/Home.tsx
@@ -0,0 +1,25 @@
+import { HiPlus } from "../constant/icons";
+import JobCard from "./Job";
+import JOBS from "../data/Jobs";
+
+const Home = () => {
+    return (
+        <div className="main-dash flex h-screen flex-col justify-center items-center">
+            <div className="top w-full px-4 py-1.5 text-center">List of Jobs</div>
+            <div className="middle h-full w-full overflow-y-auto">
+                <div className="jobs">
+                    <ul>
+                        {JOBS.map((job) => {
+                            return <JobCard key={job.id} job={job} />;
+                        })}
+                    </ul>
+                </div>
+            </div>
+            <button className="bottom w-full bg-[#1d1c21] py-2 px-4 text-white cursor-pointer flex justify-center items-center">
+                <HiPlus className={"h-10 w-10"} />
+            </button>
+        </div>
+    );
+};
+
+export default Home;
