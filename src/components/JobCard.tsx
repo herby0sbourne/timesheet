7dc66cbf8cffe6295e11405f265960deb238bf16
Herby <herbyosbourne31@gmail.com>
2022-10-11 14:58:38 -0500

    use React router Link

--- src/components/JobCard.tsx
+++ src/components/JobCard.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { Job } from "../data/Jobs";
 
 type JobProps = {
@@ -5,12 +6,14 @@ type JobProps = {
 };
 
 const JobCard = ({ job }: JobProps) => {
-    const { title } = job;
+    const { id, title } = job;
     return (
         <li>
-            <div className="job bg-[#f9f7f6] px-4 pt-4 pb-2 border-b-2 border-[#f5deb3]">
+            <Link
+                to={`/${id}`}
+                className="job bg-[#f9f7f6] block px-4 pt-4 pb-2 border-b-2 border-[#f5deb3]">
                 <p>{title}</p>
-            </div>
+            </Link>
         </li>
     );
 };
