44e67d8c762922b417c67bd4812c1701bba6f736
Herby <herbyosbourne31@gmail.com>
2022-10-17 08:57:59 -0500

    Change path to use userId to find duties

--- src/components/JobCard.tsx
+++ src/components/JobCard.tsx
@@ -6,11 +6,11 @@ type JobProps = {
 };
 
 const JobCard = ({ job }: JobProps) => {
-    const { id, title } = job;
+    const { title, userId } = job;
     return (
         <li>
             <Link
-                to={`/${id}`}
+                to={`/${userId}`}
                 className="job bg-[#f9f7f6] block px-4 pt-4 pb-2 border-b-2 border-[#f5deb3]">
                 <p>{title}</p>
             </Link>
