ef9011653c53df97f93f2cd27b3896375ad87809
Herby <herbyosbourne31@gmail.com>
2022-10-17 09:01:51 -0500

    Restructure folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import StoreProvider from "./context/Store";
+import StoreProvider from "./provider/Store";
 import App from "./App";
 
 import "./index.css";
