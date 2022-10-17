a2078b805ce6430277387b0a77eb6495a87893f8
Herby <herbyosbourne31@gmail.com>
2022-10-17 09:04:26 -0500

    Create sample data

---
+++ src/data/DUTY.ts
@@ -0,0 +1,90 @@
+import { v4 as uuidv4 } from "uuid";
+
+const DUTY = [
+    {
+        id: uuidv4(),
+        userId: 2,
+        clockInDay: "Sun September 24, 2222",
+        clockOutDay: "Sun September 25, 2222",
+        clockedIn: "7:00 AM",
+        clockedOut: "7:00 PM",
+        pay: 3744.0,
+        location: "cotton factory",
+        hoursWorked: 12,
+        rate: 310
+    },
+    {
+        id: uuidv4(),
+        userId: 2,
+        clockInDay: "Sun September 25, 2222",
+        clockOutDay: "Sun September 25, 2222",
+        clockedIn: "7:00 AM",
+        clockedOut: "7:00 PM",
+        pay: 3744.0,
+        location: "cotton factory",
+        hoursWorked: 12,
+        rate: 310
+    },
+    {
+        id: uuidv4(),
+        userId: 3,
+        clockInDay: "Sun September 26, 2222",
+        clockOutDay: "Sun September 25, 2222",
+        clockedIn: "7:00 AM",
+        clockedOut: "7:00 PM",
+        pay: 3744.0,
+        location: "cotton factory",
+        hoursWorked: 12,
+        rate: 310
+    },
+    {
+        id: uuidv4(),
+        userId: 2,
+        clockInDay: "Sun September 27, 2222",
+        clockOutDay: "Sun September 25, 2222",
+        clockedIn: "7:00 AM",
+        clockedOut: "7:00 PM",
+        pay: 3744.0,
+        location: "cotton factory",
+        hoursWorked: 12,
+        rate: 310
+    },
+    {
+        id: uuidv4(),
+        userId: 3,
+        clockInDay: "Sun September 28, 2222",
+        clockOutDay: "Sun September 25, 2222",
+        clockedIn: "7:00 AM",
+        clockedOut: "7:00 PM",
+        pay: 3744.0,
+        location: "cotton factory",
+        hoursWorked: 12,
+        rate: 310
+    },
+    {
+        id: uuidv4(),
+        userId: 2,
+        clockInDay: "Sun September 29, 2222",
+        clockOutDay: "Sun September 25, 2222",
+        clockedIn: "7:00 AM",
+        clockedOut: "7:00 PM",
+        pay: 3744.0,
+        location: "cotton factory",
+        hoursWorked: 12,
+        rate: 310
+    },
+    {
+        id: uuidv4(),
+        userId: 3,
+        clockInDay: "Sun September 30, 2222",
+        clockOutDay: "Sun September 25, 2222",
+        clockedIn: "7:00 AM",
+        clockedOut: "7:00 PM",
+        pay: 3744.0,
+        location: "cotton factory",
+        hoursWorked: 12,
+        rate: 310
+    }
+];
+
+export default DUTY;
