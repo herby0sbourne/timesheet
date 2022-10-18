0bb0a267c83988c165a01c76adc99240316c0392
Herby <herbyosbourne31@gmail.com>
2022-10-18 12:45:56 -0500

    create CreateNewJob component

--- src/components/CreateJob.tsx
+++ src/components/CreateJob.tsx
@@ -1,5 +1,33 @@
+import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { StoreContext } from "../provider/Store";
+import InputGroup from "./InputGroup";
+
 const CreateJob = () => {
-    return <div>Create Job</div>;
+    const navigate = useNavigate();
+    const [error, setError] = useState("");
+    const { createNewJob } = useContext(StoreContext);
+
+    const onSubmitHandler = (e: React.SyntheticEvent) => {
+        e.preventDefault();
+        const target = e.target as typeof e.target & {
+            job: { value: string };
+        };
+        const job = target.job.value;
+        if (job === "") return setError("invalid Job Title");
+        createNewJob(job);
+        navigate("/");
+    };
+
+    return (
+        <form className="px-4 text-center" onSubmit={onSubmitHandler}>
+            {error && <p>{error}</p>}
+            <InputGroup type="text" name="job" placeholder="Job Title" />
+            <button className="p-2 bg-pink-300 rounded-lg mt-4 mx-auto" type="submit">
+                Submit
+            </button>
+        </form>
+    );
 };
 
 export default CreateJob;
