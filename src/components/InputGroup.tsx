47e570e76585ad655fd44c8c3149b027e0a8a6b0
Herby <herbyosbourne31@gmail.com>
2022-10-18 12:48:19 -0500

    add name to InputGroup

--- src/components/InputGroup.tsx
+++ src/components/InputGroup.tsx
@@ -1,14 +1,16 @@
 interface InputGroupProps {
+    name: string;
     type: string;
     placeholder: string;
 }
 
-const InputGroup = ({ type, placeholder }: InputGroupProps) => {
+const InputGroup = ({ name, type, placeholder }: InputGroupProps) => {
     return (
         <div className="input-group rounded-[4px] mt-4 border border-[gray] text-green-500">
             <input
                 className={"bg-transparent px-3 py-1.5 w-full"}
                 type={type}
+                name={name}
                 placeholder={placeholder}
                 // value={300}
             />
