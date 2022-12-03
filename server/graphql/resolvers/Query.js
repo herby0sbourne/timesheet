5ad0a404184e44ff95385225a4e1c8530ce16dae
Herby <herbyosbourne31@gmail.com>
2022-12-03 17:08:02 -0500

    remove sample data

--- server/graphql/resolvers/Query.js
+++ server/graphql/resolvers/Query.js
@@ -1,32 +1,4 @@
-import { v4 as uuidv4 } from 'uuid';
-
-const users = [
-  {
-    userName: 'sam',
-    id: '1',
-  },
-  {
-    userName: 'smith',
-    id: '2',
-  },
-  {
-    userName: 'will',
-    id: '3',
-  },
-];
-
-const jobs = [
-  {
-    id: uuidv4(),
-    userId: '2',
-    title: 'Security Duties',
-  },
-  {
-    id: uuidv4(),
-    userId: '3',
-    title: 'Studio managers',
-  },
-];
+import {jobs,users,duties} from '../../database/database.js'
 
 const Query = {
   users: (parent, args, context, info) => {
@@ -36,10 +8,8 @@ const Query = {
   },
   user: (parent, args, ctx, info) => {
     const user = users.find((user) => user.id === args.id);
-
     return user;
   },
-
   jobs: (parent, args, ctx, info) => {
     return jobs;
   },
@@ -47,6 +17,13 @@ const Query = {
     const job = jobs.find((job) => job.userId === args.id);
     return job;
   },
+  duty:(parent, args, ctx, info) =>{
+    const duty = duties.find((duty) => duty.id === args.id)
+    return duty
+  },
+  duties:(parent, args, ctx, info) =>{
+    return duties
+  }
 };
 
 export default Query;
